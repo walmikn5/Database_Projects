417cdb428032814eab7f59cd00688caa1b9ce294
Walmik Nirvikar <85093153+walmikn5@users.noreply.github.com>
2023-05-28 16:03:45 +0530

    Create Table

---
+++ Blood_Donation_System/Table.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `tblblooddonars` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(100) DEFAULT NULL,
+  `MobileNumber` char(11) DEFAULT NULL,
+  `EmailId` varchar(100) DEFAULT NULL,
+  `Gender` varchar(20) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `BloodGroup` varchar(20) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `Message` mediumtext,
+  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(1) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `tblbloodgroup` (
+  `id` int(11) NOT NULL,
+  `BloodGroup` varchar(20) DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `tblcontactusinfo` (
+  `id` int(11) NOT NULL,
+  `Address` tinytext,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `ContactNo` char(11) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `tblcontactusquery` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `EmailId` varchar(120) DEFAULT NULL,
+  `ContactNumber` char(11) DEFAULT NULL,
+  `Message` longtext,
+  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `tblpages` (
+  `id` int(11) NOT NULL,
+  `PageName` varchar(255) DEFAULT NULL,
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `detail` longtext NOT NULL
+);
